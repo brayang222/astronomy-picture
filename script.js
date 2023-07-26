af26b873d67924da1ed2c806897aa95a6598065f
brayang222 <116692605+brayang222@users.noreply.github.com>
2023-07-25 19:39:41 -0500

    add traduction

--- script.js
+++ script.js
@@ -1,42 +1,92 @@
 const key = 'THrvkyf0glfmOsl49ezraK3pMTY8UJ1ZlcQwDP1L';
-const url = `https://api.nasa.gov/planetary/apod?api_key=${key}`;
+const urlNasa = `https://api.nasa.gov/planetary/apod?api_key=${key}`;
+const btn = document.createElement('button');
 
-fetch(url)
+// ------------- API NASA --------------------------------
+
+fetch(urlNasa)
 .then(resp => resp.json())
 .then(data => {
-    const photoContainer = document.getElementById('photoContainer');
-    const img = document.createElement('img');
-    const input = document.createElement('input');
-    const expli = document.createElement('h3')
+  const photoContainer =
+    document.getElementById(
+      "photoContainer"
+    );
+  const img =
+    document.createElement("img");
+  const input =
+    document.createElement("input");
+  const expli =
+    document.createElement("h3");
 
+  input.type = "date";
+  img.src = data.hdurl;
+  expli.textContent = data.explanation;
+  photoContainer.appendChild(img);
+  photoContainer.appendChild(input);
+  photoContainer.appendChild(btn);
+  photoContainer.appendChild(expli);
 
-    input.type = 'date';
-    img.src = data.hdurl;
-    expli.textContent = data.explanation;
-    photoContainer.appendChild(img);
-    photoContainer.appendChild(input);
-    photoContainer.appendChild(expli);
-    
+  input.addEventListener(
+    "change",
+    function () {
+      const selectedDate = this.value;
+      const updatedUrl = `${urlNasa}&date=${selectedDate}`;
 
-    input.addEventListener('change', function() {
-        const selectedDate = this.value;
-        const updatedUrl = `https://api.nasa.gov/planetary/apod?api_key=${key}&date=${selectedDate}`;
-
-        fetch(updatedUrl)
-        .then(resp => resp.json())
-        .then(data => {
-            img.src = data.url;
-            expli.textContent = data.explanation;
-
+      fetch(updatedUrl)
+        .then((resp) => resp.json())
+        .then((data) => {
+          img.src = data.url;
+          expli.textContent =
+            data.explanation;
         })
-        .catch(error => {
-            console.log('Error al cargar la imagen:', error);
+        .catch((error) => {
+          console.log(
+            "Error al cargar la imagen:",
+            error
+          );
         });
-    });
+    }
+  );
+
+  // ----------------- TRADUCTOR --------------------------
+  let en = false;
+
+  btn.classList.add("btn");
+  btn.innerHTML =
+    '<i class="fa-solid fa-repeat"></i><h2>es</h2>';
+
+  btn.addEventListener("click", () => {
+    en = !en;
+
+    const url = 'https://text-translator2.p.rapidapi.com/translate';
+    const options = {
+	    method: 'POST',
+	    headers: {
+		   'content-type': 'application/x-www-form-urlencoded',
+		   'X-RapidAPI-Key': 'd432e9af6bmsh90229f604ceafd1p16b94cjsnfd0f82b2e7cc',
+		    'X-RapidAPI-Host': 'text-translator2.p.rapidapi.com'
+	    },
+	    body: new URLSearchParams({
+		    source_language: 'en',
+	  	  target_language: 'es',
+		    text: data.explanation
+  	  })
+    };
+    fetch(url, options)
+      .then((resp) => resp.json())
+      .then((data) => {
+        expli.textContent = data.data.translatedText;
+      });
+    en === true
+      ? btn.innerHTML = '<i class="fa-solid fa-repeat"></i><h2>en</h2>'
+      : btn.innerHTML = '<i class="fa-solid fa-repeat"></i><h2>es</h2>';
+  });
 })
 .catch(error => {
     console.log('Error al obtener los datos de la API:', error);
 });
+
+
 
 // ------------------ STARS ---------------
 
@@ -62,8 +112,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const x = Math.random() * 100;
     const y = Math.random() * 100;
-    star.style.left = x + 'vw';
-    star.style.top = y + 'vh';
+    star.style.left = x + '%';
+    star.style.top = y + '%';
 
     // Agregar un retraso aleatorio a la animación de cada estrella
     star.style.animationDelay = getRandomDelay() + 's';
@@ -71,8 +121,10 @@ document.addEventListener("DOMContentLoaded", () => {
     starsContainer.appendChild(star);
   }
 
-  const numGalaxies = 10; // Número de galaxias diferentes
-  const galaxySizeRange = { min: 20, max: 50 }; // Rango de tamaño de galaxias en píxeles
+  // --------------------------- GALAXIES ------------------
+
+  const numGalaxies = 20; // Número de galaxias diferentes
+  const galaxySizeRange = { min: 5, max: 35 }; // Rango de tamaño de galaxias en píxeles
 
   // Array con las rutas de las imágenes de galaxias
   const galaxyImages = [
@@ -86,8 +138,6 @@ document.addEventListener("DOMContentLoaded", () => {
     "galaxias/galaxia8.jpeg",
     "galaxias/galaxia4.jpeg",
     "galaxias/galaxia3.jpg"
-    // Agrega aquí las rutas de las demás imágenes de galaxias
-    // Por ejemplo: "ruta/a/tu/galaxia-3.jpg", "ruta/a/tu/galaxia-4.jpg", ...
   ];
 
   const galaxiesContainer = document.getElementById('galaxies');
@@ -111,8 +161,8 @@ document.addEventListener("DOMContentLoaded", () => {
     galaxy.style.height = size + 'px';
 
     const { x, y } = getRandomPosition();
-    galaxy.style.left = x + 'vw';
-    galaxy.style.top = y + 'vh';
+    galaxy.style.left = x + '%';
+    galaxy.style.top = y + '%';
 
     const randomIndex = Math.floor(Math.random() * galaxyImages.length);
     galaxy.style.backgroundImage = `url(${galaxyImages[randomIndex]})`;
